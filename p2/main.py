7a802968adead8be9ade1bd04e615b3641c12e25
communist-funny-man <danidezaprieto@gmail.com>
2023-09-28 11:46:36 +0200

    tuples and list multiplication

--- p2/main.py
+++ p2/main.py
@@ -1,1 +1,6 @@
 # List datatype: structure or class that stores a sequence of elements
+# Tuples: Non-mutable lists
+tupl = (1,2,3)
+# if lists are multiplied it will create 2 copies of the list and place them in a new one 
+#l = [1,2,3]
+#l*2 = [1,2,3,1,2,3]
