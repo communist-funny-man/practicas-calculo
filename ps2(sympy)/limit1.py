c33af51bbda3a725ca229a7b441493c296b9bfca
communist-funny-man <danidezaprieto@gmail.com>
2023-10-06 12:58:51 +0200

    Added function computing example

--- ps2(sympy)/limit1.py
+++ ps2(sympy)/limit1.py
@@ -1,2 +1,7 @@
 import sympy as sp
-x=sp.S('x')
+x=sp.S('x') #symbolic variable defined
+#compute values of the symbolic function with subs
+#substitutes x by 1
+sp.Subs(x,1)
+f=(sp.Abs(2*x-1)-sp.Abs(2*x+1))/x #symbolic function given by expression
+print(f)
