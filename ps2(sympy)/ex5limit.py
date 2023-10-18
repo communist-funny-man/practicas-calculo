2b68324121cd602ade6c910f98212a93f57fcabe
communist-funny-man <danidezaprieto@gmail.com>
2023-10-18 12:37:36 +0200

    Updated repo

---
+++ ps2(sympy)/ex5limit.py
@@ -0,0 +1,11 @@
+import sympy as sp
+x=sp.S('X')
+f=(3*x**2-2)/(4*x**2+1)
+sp.plot(f)
+#horizontal asintote computation
+print('limit at infinity',sp.limit(f,x,'+oo'))
+print('limit at infinity',sp.limit(f,x,'-oo'))
+#two horizontal asintotes exist at 3/4 on both sides of the limit
+rightasint = sp.limit(f,x,'+oo')
+leftasint = sp.limit(f,x,'-oo')
+sp.plot(f,rightasint)
--- ps2(sympy)/limit1.py
+++ ps2(sympy)/limit1.py
@@ -1,7 +1,13 @@
 import sympy as sp
 x=sp.S('x') #symbolic variable defined
+f=(sp.Abs(2*x-1)-sp.Abs(2*x+1))/x #symbolic function given by expression
+f.subs(x,1)
 #compute values of the symbolic function with subs
 #substitutes x by 1
-sp.Subs(x,1)
-f=(sp.Abs(2*x-1)-sp.Abs(2*x+1))/x #symbolic function given by expression
 print(f)
+#left sided limit of function f
+print('left sided limit is', sp.limit(f,x,0,dir='-'))
+print('Right sided limit is',sp.limit(f,x,0,dir='+'))
+sp.plot(f)
+g=x**2
+sp.plot(f,g,ylim=[-5,1],xlim=[-5,5])
